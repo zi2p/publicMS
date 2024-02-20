61b96e53312695186782e659f94ecac625cecf81
Danil Ivanov <113857447+daanil-ivanov@users.noreply.github.com>
2024-02-20 17:03:41 +0300

    Update NISO1.js

--- script/NISO1.js
+++ script/NISO1.js
@@ -4,14 +4,19 @@ const plusBtn = document.getElementById('plus_btn');
 const minusBtn = document.getElementById('minus_btn');
 let zoom = window.screen.width > 432 ? 2.8 : 1.6;
 let customLatitude = null;
+let customLongitude = null;
+let marker
 
 const map = WE.map('map', { zoom: zoom, dragging: true, scrollWheelZoom: true });
 
 function init() {
     map.on("click", function (e) {
         customLatitude = e.latlng.lat.toFixed(5);
-        latitudeInput.value = customLatitude;
-    });
+        customLongitude = e.latlng.lng.toFixed(5);
+        latitudeInput.value = customLatitude
+        // addMarker(e.latlng.lat.toFixed(5), e.latlng.long.toFixed(5))
+        addMarker()
+    })
 
     WE.tileLayer('https://webglearth.github.io/webglearth2-offline/{z}/{x}/{y}.jpg', { tileSize: 200, tms: true }).addTo(map);
 }
@@ -30,67 +35,50 @@ function calculateGravity() {
 
     const gravity = calculateGravityAtLatitude(latitude);
     resultElement.textContent = `На широте ${latitude}° g=${gravity.toFixed(3)} м/с²`;
-
-    // Сохранение результата в локальное хранилище
-    saveToLocalStorage(latitude, gravity.toFixed(3));
 }
 
 function calculateGravityAtLatitude(latitude) {
     const g0 = 9.78; // Ускорение свободного падения на экваторе
     const omegaSquared = 7.2921159e-5 ** 2; // Квадрат угловой скорости вращения Земли
-    const sinSquared = Math.sin(latitude * (Math.PI / 180)) ** 2; // Квадрат
+    const sinSquared = Math.sin(latitude * (Math.PI / 180)) ** 2; // Квадрат 
     const eSquared = 0.00669438 // Квадрат эксцентриситета Земли
 
     return g0 * (1 - omegaSquared * sinSquared) / Math.sqrt(1 - eSquared * sinSquared);
 }
 
-// Сохранение в локальное хранилище
-function saveToLocalStorage(latitude, gravity) {
-    localStorage.setItem("latitude", latitude);
-    localStorage.setItem("gravity", gravity);
-}
-
 function zoomIn() {
-    zoom = map.getZoom();
-    zoom += 0.2;
-    map.setZoom(zoom);
+    zoom = map.getZoom()
+    zoom += 0.2
+    map.setZoom(zoom)
 }
 
 function zoomOut() {
-    zoom = map.getZoom();
-    zoom -= 0.2;
-    map.setZoom(zoom);
+    zoom = map.getZoom()
+    zoom -= 0.2
+    map.setZoom(zoom)
 }
 
 function rotateTop() {
-    const pos = map.getPosition();
-    map.setCenter([pos[0] + 10, pos[1]]);
+    const pos = map.getPosition()
+    map.setCenter([pos[0] + 10, pos[1]])
 }
 
 function rotateBottom() {
-    const pos = map.getPosition();
-    map.setCenter([pos[0] - 10, pos[1]]);
+    const pos = map.getPosition()
+    map.setCenter([pos[0] - 10, pos[1]])
 }
 
 function rotateLeft() {
-    const pos = map.getPosition();
-    map.setCenter([pos[0], pos[1] - 10]);
+    const pos = map.getPosition()
+    map.setCenter([pos[0], pos[1] - 10])
 }
 
 function rotateRight() {
-    const pos = map.getPosition();
-    map.setCenter([pos[0], pos[1] + 10]);
+    const pos = map.getPosition()
+    map.setCenter([pos[0], pos[1] + 10])
 }
 
-// Вызов функции загрузки при загрузке страницы
-window.onload = function () {
-    init();
-
-    const savedLatitude = localStorage.getItem("latitude");
-    const savedGravity = localStorage.getItem("gravity");
-
-    if (savedLatitude && savedGravity) {
-        latitudeInput.value = savedLatitude;
-        resultElement.textContent = `На широте ${savedLatitude}° g=${savedGravity} м/с²`;
-    }
-};
+function addMarker() {
+    if (marker) marker.removeFrom(map)
+    marker = WE.marker([customLatitude, customLongitude]).addTo(map);
+}
